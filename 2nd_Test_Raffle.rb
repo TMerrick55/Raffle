c2b6a80991dd2ec630d5dd77ead26c2cc4a212c0
TMerrick55 <tmerrick44@gmail.com>
2017-01-29 17:03:38 -0500

    Raffle HW: Part 2, Ver. 2.0

--- 2nd_Test_Raffle.rb
+++ 2nd_Test_Raffle.rb
@@ -5,4 +5,8 @@ class TestMMFunctions < Minitest::Test
 	def test_same_number_returns_false
 		assert_equal(['False'], off_by_one(1234,1234))
 	end
+
+	def test_different_number_returns_true
+		assert_equal(['True'], off_by_one(6247,6257))
+	end
 end
